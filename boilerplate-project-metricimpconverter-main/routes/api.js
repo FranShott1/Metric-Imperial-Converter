e706ec2ca9e3637576b9670868ca1a163e723777
FranShott1 <167144173+FranShott1@users.noreply.github.com>
2025-10-27 15:03:25 -0300

    Update api.js

--- boilerplate-project-metricimpconverter-main/routes/api.js
+++ boilerplate-project-metricimpconverter-main/routes/api.js
@@ -7,19 +7,26 @@ module.exports = function (app) {
   let convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
-    .get(function (req, res) {
-      let input = req.query.input;
-      let initNum = convertHandler.getNum(input);
-      let initUnit = convertHandler.getUnit(input);
+  .get(function (req, res) {
+    let input = req.query.input;
+    let initNum = convertHandler.getNum(input);
+    let initUnit = convertHandler.getUnit(input);
 
-      if (!initNum && !initUnit) return res.send('invalid number and unit');
-      if (!initNum) return res.send('invalid number');
-      if (!initUnit) return res.send('invalid unit');
+    // Verificar errores
+    if (initNum === null && initUnit === null) {
+      return res.send('invalid number and unit');
+    }
+    if (initNum === null) {
+      return res.send('invalid number');
+    }
+    if (initUnit === null) {
+      return res.send('invalid unit');
+    }
 
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+    let returnNum = convertHandler.convert(initNum, initUnit);
+    let returnUnit = convertHandler.getReturnUnit(initUnit);
+    let string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-      res.json({ initNum, initUnit, returnNum, returnUnit, string });
-    });
+    res.json({ initNum, initUnit, returnNum, returnUnit, string });
+     });
 };
